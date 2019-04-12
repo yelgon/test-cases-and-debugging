49c82c328c38586bb38fa1e6013a90e58d5f1905
Jordan - Instructor <jordan-instructor@Jordans-MacBook-Pro.local>
2019-04-11 21:01:40 -0400

    Added better error message

--- problems/verify-equals.js
+++ problems/verify-equals.js
@@ -2,6 +2,7 @@ let equal = require('deep-equal');
 // The verifyEquals function checks that two values contain equivalent data. Whereas === only works for primitives, verifyEquals also works for objects (including arrays).
 let verifyEquals = (a, b) => {
   if (!equal(a, b)) {
+    console.error('Expected:', a, 'Actual:', b);
     throw new Error('Equality test failed');
   }
 };
